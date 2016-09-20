fbf3b6f36581cc855cff8e7a136bfeba31c41129
reza gorjian <reza7380@gmail.com>
2016-09-20 13:57:13 +0000

    Update rebot.lua

--- plugins/rebot.lua
+++ plugins/rebot.lua
@@ -1,20 +1,9 @@
---[[
-#
-#ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
-#:((
-# For More Information ....! 
-# author: Aziz < @TH3_GHOST > 
-# our channel: @DevPointTeam
-# Version: 1.1
-#:))
-#ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
-#
-]]
+
 do 
 local function run(msg, matches) 
 if ( msg.text ) then
   if ( msg.to.type == "user" ) then
-     return "- Welcome to DevPoint !\n\n- For More Information Subscribe in \n\n- Channel : @DevPointTeam"
+     return "- Welcome to BombusMOD !\n\n- For More Information Subscribe in \n\n- Channel : @Ntflight"
     end 
   end 
 end 
