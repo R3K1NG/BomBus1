a2ac2a14b57eb523cd47959f1c535097c54029af
reza gorjian <reza7380@gmail.com>
2016-09-20 13:58:28 +0000

    Update serverinfo.lua

--- plugins/serverinfo.lua
+++ plugins/serverinfo.lua
@@ -1,15 +1,4 @@
---[[
-#
-#ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
-#:((
-# For More Information ....! 
-# Developer : Aziz < @TH3_GHOST > 
-# our channel: @DevPointTeam
-# Version: 1.1
-#:))
-#ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
-#
-]]
+
 local function run(msg, matches)
 local text = io.popen("sh ./data/cmd.sh"):read('*all')
 if is_sudo(msg) then
